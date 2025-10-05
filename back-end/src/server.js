3eac7da458f850d2434ec909e851a4c1a64ad9e9
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-05 17:12:29 +0530

    create endpoints 2.2

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());    //to enable request body parsing
+
 app.get('/hello', function(req, res) {
     res.send('Hello');
+})
+
+app.post('/hello', function(req, res) {
+    res.send('Hello ' + req.body.name + ' from the POST method');
 })
 
 app.listen(8000, function() {
